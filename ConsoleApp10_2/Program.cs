a7cc99f1906ccf77ccf3c996f02389033387e65e
corssson <vanyakirill@bk.ru>
2025-12-21 18:31:21 +0500

    задание10_3

---
+++ ConsoleApp10_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число a (1 < a < 2): ");
+        double a = double.Parse(Console.ReadLine());
+
+        int n = 1;
+        while (true)
+        {
+            double value = 1.0 + 1.0 / n;
+
+            if (value < a)
+                break;
+
+            n++;
+        }
+
+        Console.WriteLine($"Первое число последовательности меньше a: 1 + 1/{n}");
+    }
+}
