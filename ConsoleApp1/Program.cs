c406cc69716834bfafebcc15bce0ba22c589c5b4
corssson <vanyakirill@bk.ru>
2025-12-21 17:58:08 +0500

    задание 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ColoredPoem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Александр Пушкин");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("«Я помню чудное мгновенье…»\n");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Я помню чудное мгновенье:");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Передо мной явилась ты,");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Как мимолётное виденье,");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Как гений чистой красоты.");
+
+            Console.ResetColor();
+        }
+    }
+}
