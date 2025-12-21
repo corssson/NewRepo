402660702368707f30361a26c9fc3cd3b3353290
corssson <vanyakirill@bk.ru>
2025-12-21 18:16:05 +0500

    задание 9

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение аргумента");
+
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"f({x:F2}) = {F(x):F2}");
+        }
+
+        static double F(double x)
+        {
+            if (x > 2)
+                return 2;
+            else if (x >= -2)
+                return x;
+            else
+                return -2;
+        }
+    }
+}
